2d830f3c840e069ca23b57ed8301de4310d1210d
dkhos17 <dkhos17@freeuni.edu.ge>
2021-04-27 22:14:07 +0400

    challenge-1

--- tests/pokerdevs/event_tuple/test_event_tuple.py
+++ tests/pokerdevs/event_tuple/test_event_tuple.py
@@ -7,13 +7,17 @@ logger = logging.getLogger(__name__)
 
 
 def generate_test_events():
-    raise NotImplementedError
+    yield ("b5796505-68fd-40d5-814a-9d31f3f084b0", 1619146052, "UserClickedOnButton", "close_button")
+    yield ("1d0bfda2-fe1d-4c61-a8bb-2c7df89beddf", 1619146123, "UserLongPressed", 340, 420)
 
 def serialize_event(event):
-    raise NotImplementedError
+    return f"Event '{event[2]}' occurred at {event[1]} with ID: {event[0]}"
 
 
 
 def test_event_tuple():
     logger.info(f"Starting the test !")
-    pytest.fail(f"YOU NEED TO FINISH THIS")
+    for event in generate_test_events():
+        print(serialize_event(event))
+
+    # pytest.fail(f"YOU NEED TO FINISH THIS")
